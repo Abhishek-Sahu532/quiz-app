3c20831692d67f852005f5658f63162bfc3be765
Abhishek Sahu <asahu532@gmail.com>
2023-06-29 20:14:01 +0530

    high scores tab completed

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ let currentQuestion = 0;
 let score = 0;
 let userName = "";
 let quizData = [];
-let hightScore = 0;
+const scores = JSON.parse(localStorage.getItem('scores')) || [];
 let selectedOption = ""; //SELECTED VALUE BY USER
 
 //START QUIZ
 
 async function loadQuizData() {
+const scores = JSON.parse(localStorage.getItem('scores')) || [];
   console.log("Loading the questions");
   const res = await fetch("questions.json");
   quizData = await res.json();
@@ -103,12 +104,13 @@ document.getElementById("next-btn").addEventListener("click", (e) => {
   }
 });
 
+
+
 function endQuiz() {
   document.getElementById("quiz-container").style.display = "none";
   document.getElementById("score-container").style.display = "block";
   document.getElementById('final-score').innerText = score;
 
-const scores = JSON.parse(localStorage.getItem('scores')) || [];
 
 scores.push({
     userName: userName,
@@ -140,18 +142,28 @@ document.getElementById('restart-btn').addEventListener('click',()=>{
 
 //highscores
 
- 
-document.getElementById('highscore-btn').addEventListener('click',(e)=>{
-document.getElementById('highscore-page').style.display ='block';
-document.getElementById('start-page').style.display ='none';
 
-const scores = JSON.parse(localStorage.getItem('scores'));
-scores.map((item)=>{
-`<p>${item.userName} : ${item.score} on ${new Date(item.date).toLocaleDateString()} at ${new Date(
-    item.date
-).toLocaleTimeString()}
+// Function to show high scores page with all the scores from local storage
+const showHighscores = () => {
+  document.getElementById("start-page").style.display = "none";
+  document.getElementById("highscore-page").style.display = "block";
+  document.getElementById("highscores").innerHTML = scores
+    .map(
+      (item) =>
+        `<p>${item.userName}: ${item.score} (on ${new Date(
+          item.date
+        ).toLocaleDateString()} at ${new Date(
+          item.date
+        ).toLocaleTimeString()})</p>`
+    )
+    .join("");
 
+  if (scores.length == 0) {
+    document.getElementById("highscores").innerHTML =
+      "<h3>No Scores Yet!</h3><h4>Play the game to see your score's here.</h4>";
+  }
+};
 
-</p>`
-} )
-})
+document
+  .getElementById("highscore-btn")
+  .addEventListener("click", showHighscores);
